afc0e32c407143dd5e8f030b20c1219cbb25cd97
boilerkeeper <simonwright81@gmail.com>
2017-05-17 18:11:35 +0100

    First Commit

---
+++ Assets/Controllers/WorldController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+	public GameObject dustguyPrefab;
+	GameObject[] charPool;
+	int charIndex = 0;
+	int maxChars = 50;
+
+	public void Start ()
+	{
+		charPool = new GameObject[maxChars];
+	}
+		
+	public void Update ()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			CastRayToWorld ();
+		}
+	}
+
+	void CastRayToWorld ()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Vector2 point = ray.origin + (ray.direction * 4.5f);
+		createAtMouseClick (point);
+	}
+
+	void createAtMouseClick (Vector2 initPos)
+	{
+		// If object already exists at this array index > destroy
+		if (charPool [charIndex] != null) {
+			Destroy (charPool [charIndex]);
+		}
+		// Instantiate new character at mouse pos
+		GameObject newChar;
+		newChar = Instantiate(dustguyPrefab, initPos, Quaternion.identity);
+		// Make it a child of WorldController
+		newChar.transform.parent = this.transform;
+		// Rename
+		newChar.transform.name = "dust#" + charIndex;
+		// Add to array
+		charPool [charIndex] = newChar;
+		// Loop array index
+		if (charIndex < maxChars - 1)
+		{
+			charIndex++;
+		}
+		else 
+		{
+			charIndex = 0;
+		}
+	}
+}
---
+++ Assets/DataClasses/DustMote.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustMote : MonoBehaviour
+{
+	Vector2 speed;
+	float timeStamp;
+	float coolDownTime = 0.2f;
+
+	public void Awake () 
+	{
+		timeStamp = Time.time + 0.5f;
+		randomizeVelocity ();	
+	}
+
+	void Update ()
+	{
+		Vector2 currentPos;
+
+		currentPos = this.transform.position;
+		currentPos += (speed * Time.deltaTime);
+
+		// Check for exceeding boundaries
+		if (currentPos.x > 10f || currentPos.x < -10f)
+		{
+			currentPos.x = -currentPos.x;
+		}
+		if (currentPos.y > 7f || currentPos.y < -7f)
+		{
+			currentPos.y = -currentPos.y;
+		}
+		// Update changes
+		this.transform.position = currentPos;
+	}
+
+	void OnMouseOver ()
+	{
+		// Left mouse click randomises velocity
+		if (timeStamp <= Time.time) {
+			randomizeVelocity ();
+			timeStamp = Time.time + coolDownTime;
+		}
+
+
+	}
+
+	void randomizeVelocity (){
+		speed = new Vector2 (Random.Range(-2f,2f),Random.Range(-2f,2f));
+
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play ();
+
+	}
+}
